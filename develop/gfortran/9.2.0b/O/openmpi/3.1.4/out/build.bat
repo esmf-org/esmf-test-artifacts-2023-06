334c1b2177222bd9751948aa6b43644b5a0ece13
Gerhard Theurich <theurich@sourcespring.net>
2023-04-25 07:52:19 +0000

    action=collect dir=gfortran_9.2.0b_openmpi_O_develop branch=develop hash=v8.5.0b19-5-gc2c4406 phase=build

---
+++ develop/gfortran/9.2.0b/O/openmpi/3.1.4/out/build.bat
@@ -0,0 +1,29 @@
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0b_openmpi_O_develop/build.bat_%j.o
+#SBATCH -e /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0b_openmpi_O_develop/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake
+export ESMF_MPIRUN=mpirun.srun
+module load gnu/9.2.0 openmpi/3.1.4
+
+set -x
+export ESMF_DIR=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0b_openmpi_O_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0b_openmpi_O_develop/module-build.log
+cd /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0b_openmpi_O_develop/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 40 2>&1| tee ../build.log
