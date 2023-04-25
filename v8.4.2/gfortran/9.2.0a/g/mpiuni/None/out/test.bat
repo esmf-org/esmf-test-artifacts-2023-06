8acf8641b835e68775975f52589bcdbf77e3795a
Gerhard Theurich <theurich@sourcespring.net>
2023-04-25 20:29:52 +0000

    action=collect dir=gfortran_9.2.0a_mpiuni_g_v8.4.2 branch=v8.4.2 hash=v8.4.2 phase=test

---
+++ v8.4.2/gfortran/9.2.0a/g/mpiuni/None/out/test.bat
@@ -0,0 +1,30 @@
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0a_mpiuni_g_v8.4.2/test.bat_%j.o
+#SBATCH -e /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0a_mpiuni_g_v8.4.2/test.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load gnu/9.2.0 
+module load netcdf/4.7.2
+
+set -x
+export ESMF_DIR=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0a_mpiuni_g_v8.4.2/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0a_mpiuni_g_v8.4.2/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0a_mpiuni_g_v8.4.2/module-test.log
+cd /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0a_mpiuni_g_v8.4.2/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
