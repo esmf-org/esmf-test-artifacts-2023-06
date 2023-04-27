8d86a6db88d801037de74d7f9ff7e307a811296e
Mark A Potts <Mark.Potts@noaa.gov>
2023-04-27 05:50:25 +0000

    action=collect dir=nvhpc_22.2_intelmpi_O_develop branch=develop hash=v8.5.0b20 phase=test

---
+++ develop/nvhpc/22.2/O/intelmpi/2022.3.0/out/test.bat
@@ -0,0 +1,34 @@
+#!/bin/sh -l
+#SBATCH --account=hfv3gfs
+#SBATCH -o /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/nvhpc_22.2_intelmpi_O_develop/test.bat_%j.o
+#SBATCH -e /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/nvhpc_22.2_intelmpi_O_develop/test.bat_%j.e
+#SBATCH --time=1:30:00
+#SBATCH --partition=xjet
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake
+export ESMF_MPIRUN=mpirun.srun
+export ESMF_F90=mpinvf90
+export ESMF_CXX=mpinvc++
+export ESMF_C=mpinvc
+module load nvhpc/22.2 impi/2022.3.0
+
+set -x
+export ESMF_DIR=/lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/nvhpc_22.2_intelmpi_O_develop/esmf
+export ESMF_COMPILER=nvhpc
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/nvhpc_22.2_intelmpi_O_develop/module-test.log
+cd /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/nvhpc_22.2_intelmpi_O_develop/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
