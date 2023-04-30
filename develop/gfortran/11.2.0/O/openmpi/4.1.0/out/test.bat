9872fdae9a53e7f3dff3a9b56aa449e3ebc20e8d
Bill Sacks <sacks@ucar.edu>
2023-04-30 01:42:24 -0600

    action=collect dir=gfortran_11.2.0_openmpi_O_develop branch=develop hash=v8.5.0b20 phase=test

---
+++ develop/gfortran/11.2.0/O/openmpi/4.1.0/out/test.bat
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module use /project/esmf/stack/modulefiles/compilers
+export ESMF_MPILAUNCHOPTIONS=--oversubscribe
+module load gcc/11.2.0 openmpi/4.1.0
+module load netcdf/4.7.4
+
+set -x
+export ESMF_DIR=/project/esmf/sacks/esmf-testing/gfortran_11.2.0_openmpi_O_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /project/esmf/sacks/esmf-testing/gfortran_11.2.0_openmpi_O_develop/module-test.log
+cd /project/esmf/sacks/esmf-testing/gfortran_11.2.0_openmpi_O_develop/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
