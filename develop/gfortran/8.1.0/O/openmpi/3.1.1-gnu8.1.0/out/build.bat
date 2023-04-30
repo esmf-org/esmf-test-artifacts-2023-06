6aee010f4305d5acd4b813af40c13c106d8b944c
Gerhard Theurich <theurich@sourcespring.net>
2023-04-30 03:34:16 -0600

    action=collect dir=gfortran_8.1.0_openmpi_O_develop branch=develop hash=v8.5.0b20 phase=build

---
+++ develop/gfortran/8.1.0/O/openmpi/3.1.1-gnu8.1.0/out/build.bat
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load CMake/3.24.3
+module load compiler/gnu/8.1.0 openmpi/3.1.1-gnu8.1.0
+module load tool/netcdf/4.6.1/gcc-8.1.0
+
+set -x
+export ESMF_DIR=/project/esmf/theurich/ESMF-Nightly-Testing/gfortran_8.1.0_openmpi_O_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /project/esmf/theurich/ESMF-Nightly-Testing/gfortran_8.1.0_openmpi_O_develop/module-build.log
+cd /project/esmf/theurich/ESMF-Nightly-Testing/gfortran_8.1.0_openmpi_O_develop/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 8 2>&1| tee ../build.log
