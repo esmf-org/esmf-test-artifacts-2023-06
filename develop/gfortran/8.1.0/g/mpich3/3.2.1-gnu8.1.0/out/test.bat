02ada89a0714329d1feb4b73d92fffd03cba7d5b
Gerhard Theurich <theurich@sourcespring.net>
2023-05-03 03:23:19 -0600

    action=collect dir=gfortran_8.1.0_mpich3_g_develop branch=develop hash=v8.5.0b20 phase=test

---
+++ develop/gfortran/8.1.0/g/mpich3/3.2.1-gnu8.1.0/out/test.bat
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load CMake/3.24.3
+module load compiler/gnu/8.1.0 mpich/3.2.1-gnu8.1.0
+module load tool/netcdf/4.6.1/gcc-8.1.0
+
+set -x
+export ESMF_DIR=/project/esmf/theurich/ESMF-Nightly-Testing/gfortran_8.1.0_mpich3_g_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpich3
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /project/esmf/theurich/ESMF-Nightly-Testing/gfortran_8.1.0_mpich3_g_develop/module-test.log
+cd /project/esmf/theurich/ESMF-Nightly-Testing/gfortran_8.1.0_mpich3_g_develop/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
