4d07de541321879b65cec177bda2c3ae0b5bfde2
Gerhard Theurich <theurich@sourcespring.net>
2023-05-04 17:55:27 +0000

    action=collect dir=intel_2022.3.0_intelmpi_g_develop branch=develop hash=v8.5.0b20 phase=all

---
+++ develop/intel/2022.3.0/g/intelmpi/2022.3.0/out/build.bat
@@ -0,0 +1,33 @@
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_2022.3.0_intelmpi_g_develop/build.bat_%j.o
+#SBATCH -e /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_2022.3.0_intelmpi_g_develop/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake
+export ESMF_MPIRUN=mpirun.srun
+export I_MPI_CXX=/apps/oneapi/compiler/2022.2.0/linux/bin/icpx
+export I_MPI_C=/apps/oneapi/compiler/2022.2.0/linux/bin/icx
+module load intel/2022.3.0 impi/2022.3.0
+module load netcdf-hdf5parallel/4.7.4
+
+set -x
+export ESMF_DIR=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_2022.3.0_intelmpi_g_develop/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_2022.3.0_intelmpi_g_develop/module-build.log
+cd /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_2022.3.0_intelmpi_g_develop/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 40 2>&1| tee ../build.log
