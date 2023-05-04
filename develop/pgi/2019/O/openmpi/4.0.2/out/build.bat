16a68a0f37e6969789e3ff6b8018ff418a474840
Mark Potts <mpotts@redlineperf.com>
2023-05-04 03:08:48 -0500

    action=collect dir=pgi_2019_openmpi_O_develop branch=develop hash=v8.5.0b20-1-g9196001 phase=build

---
+++ develop/pgi/2019/O/openmpi/4.0.2/out/build.bat
@@ -0,0 +1,33 @@
+#!/bin/sh -l
+#SBATCH --account=da-cpu
+#SBATCH -o /work/noaa/da/mpotts/sandbox/pgi_2019_openmpi_O_develop/build.bat_%j.o
+#SBATCH -e /work/noaa/da/mpotts/sandbox/pgi_2019_openmpi_O_develop/build.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=orion
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake intelpython3
+export ESMF_MPIRUN=mpirun.srun
+module load pgi/2019 openmpi/4.0.2
+module load netcdf/4.7.4
+
+set -x
+export ESMF_F90COMPILER=mpif90
+export ESMF_CXXCOMPILER=mpicxx
+export ESMF_DIR=/work/noaa/da/mpotts/sandbox/pgi_2019_openmpi_O_develop/esmf
+export ESMF_COMPILER=pgi
+export ESMF_COMM=openmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /work/noaa/da/mpotts/sandbox/pgi_2019_openmpi_O_develop/module-build.log
+cd /work/noaa/da/mpotts/sandbox/pgi_2019_openmpi_O_develop/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 40 2>&1| tee ../build.log
