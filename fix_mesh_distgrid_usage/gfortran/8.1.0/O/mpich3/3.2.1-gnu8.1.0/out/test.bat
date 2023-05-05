314f665cc57e3a1a67fd77df919f4b3f6d222ee1
Gerhard Theurich <theurich@sourcespring.net>
2023-05-05 02:33:01 -0600

    action=collect dir=gfortran_8.1.0_mpich3_O_fix_mesh_distgrid_usage branch=fix_mesh_distgrid_usage hash=v8.5.0b20-2-g807776b phase=test

---
+++ fix_mesh_distgrid_usage/gfortran/8.1.0/O/mpich3/3.2.1-gnu8.1.0/out/test.bat
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load CMake/3.24.3
+module load compiler/gnu/8.1.0 mpich/3.2.1-gnu8.1.0
+module load tool/netcdf/4.6.1/gcc-8.1.0
+
+set -x
+export ESMF_DIR=/project/esmf/theurich/ESMF-Nightly-Testing/gfortran_8.1.0_mpich3_O_fix_mesh_distgrid_usage/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpich3
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /project/esmf/theurich/ESMF-Nightly-Testing/gfortran_8.1.0_mpich3_O_fix_mesh_distgrid_usage/module-test.log
+cd /project/esmf/theurich/ESMF-Nightly-Testing/gfortran_8.1.0_mpich3_O_fix_mesh_distgrid_usage/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
