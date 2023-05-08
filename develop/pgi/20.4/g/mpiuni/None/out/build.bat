9b6496c06e33573993ca34d46c0c5fd51ae935ea
Mark.Potts <Mark.Potts@noaa.gov>
2023-05-08 16:56:11 -0400

    action=collect dir=pgi_20.4_mpiuni_g_develop branch=develop hash=v8.5.0b21 phase=test

---
+++ develop/pgi/20.4/g/mpiuni/None/out/build.bat
@@ -0,0 +1,28 @@
+#!/bin/sh -l
+#SBATCH --account=s2326
+#SBATCH -o /discover/nobackup/projects/sbu/mpotts/esmf-test/pgi_20.4_mpiuni_g_develop/build.bat_%j.o
+#SBATCH -e /discover/nobackup/projects/sbu/mpotts/esmf-test/pgi_20.4_mpiuni_g_develop/build.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load comp/pgi/20.4 
+
+set -x
+export ESMF_DIR=/discover/nobackup/projects/sbu/mpotts/esmf-test/pgi_20.4_mpiuni_g_develop/esmf
+export ESMF_COMPILER=pgi
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/discover/nobackup/projects/sbu/mpotts/esmf-test/pgi_20.4_mpiuni_g_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /discover/nobackup/projects/sbu/mpotts/esmf-test/pgi_20.4_mpiuni_g_develop/module-build.log
+cd /discover/nobackup/projects/sbu/mpotts/esmf-test/pgi_20.4_mpiuni_g_develop/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 28 2>&1| tee ../build.log
