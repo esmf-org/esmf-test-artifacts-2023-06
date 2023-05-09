da1b4fedba3a677c6cb157d3e8f2ea6bf43c59b4
Gerhard Theurich <theurich@sourcespring.net>
2023-05-09 00:24:13 -0700

    action=collect dir=intel_19.1.2_mpiuni_O_develop branch=develop hash=v8.5.0b21-19-g1e56fab phase=build

---
+++ develop/intel/19.1.2/O/mpiuni/None/out/build.bat
@@ -0,0 +1,34 @@
+#!/bin/sh -l
+#SBATCH --account=e3sm
+#SBATCH -o /global/cscratch1/sd/theurich/ESMF-Nightly-Testing/intel_19.1.2_mpiuni_O_develop/build.bat_%j.o
+#SBATCH -e /global/cscratch1/sd/theurich/ESMF-Nightly-Testing/intel_19.1.2_mpiuni_O_develop/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH -C haswell
+#SBATCH --qos=regular
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module unload darshan
+module load intel/19.1.2.254 
+module load cray-netcdf/4.6.3.2
+
+set -x
+export ESMF_NETCDF_LIBS="-lnetcdf"
+export ESMF_NETCDFF_LIBS="-lnetcdff"
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/opt/cray/pe/hdf5/1.10.5.2/INTEL/19.0/lib/pkgconfig
+export ESMF_DIR=/global/cscratch1/sd/theurich/ESMF-Nightly-Testing/intel_19.1.2_mpiuni_O_develop/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/global/cscratch1/sd/theurich/ESMF-Nightly-Testing/intel_19.1.2_mpiuni_O_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /global/cscratch1/sd/theurich/ESMF-Nightly-Testing/intel_19.1.2_mpiuni_O_develop/module-build.log
+cd /global/cscratch1/sd/theurich/ESMF-Nightly-Testing/intel_19.1.2_mpiuni_O_develop/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 32 2>&1| tee ../build.log
