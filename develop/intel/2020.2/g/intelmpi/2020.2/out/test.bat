93f9f9c5fc132b4a003287cfbb60062bf112e159
Mark Potts <mpotts@redlineperf.com>
2023-05-10 03:22:39 -0500

    action=collect dir=intel_2020.2_intelmpi_g_develop branch=develop hash=v8.5.0b21-19-g1e56fab phase=test

---
+++ develop/intel/2020.2/g/intelmpi/2020.2/out/test.bat
@@ -0,0 +1,33 @@
+#!/bin/sh -l
+#SBATCH --account=da-cpu
+#SBATCH -o /work/noaa/da/mpotts/sandbox/intel_2020.2_intelmpi_g_develop/test.bat_%j.o
+#SBATCH -e /work/noaa/da/mpotts/sandbox/intel_2020.2_intelmpi_g_develop/test.bat_%j.e
+#SBATCH --time=4:00:00
+#SBATCH --partition=orion
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake intelpython3
+module load intel/2020.2 impi/2020.2
+module load netcdf/4.7.4
+
+set -x
+export ESMF_MPIRUN=mpirun.srun
+export ESMF_DIR=/work/noaa/da/mpotts/sandbox/intel_2020.2_intelmpi_g_develop/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /work/noaa/da/mpotts/sandbox/intel_2020.2_intelmpi_g_develop/module-test.log
+cd /work/noaa/da/mpotts/sandbox/intel_2020.2_intelmpi_g_develop/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
