aafa930939334dc6a3a074af309e2f3456bef07c
Gerhard Theurich <theurich@sourcespring.net>
2023-05-11 01:49:35 -0700

    action=collect dir=gfortran_11.2.0_mpi_g_develop branch=develop hash=v8.5.0b21-20-gc92093e phase=test

---
+++ develop/gfortran/11.2.0/g/mpi/8.1.22/out/test.bat
@@ -0,0 +1,33 @@
+#!/bin/sh -l
+#SBATCH --account=e3sm
+#SBATCH -o /global/cfs/cdirs/e3sm/theurich/ESMF-Nightly-Testing/gfortran_11.2.0_mpi_g_develop/test.bat_%j.o
+#SBATCH -e /global/cfs/cdirs/e3sm/theurich/ESMF-Nightly-Testing/gfortran_11.2.0_mpi_g_develop/test.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH -C cpu
+#SBATCH --qos=regular
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load PrgEnv-gnu cpu
+module load gcc/11.2.0 cray-mpich/8.1.22
+module load cray-hdf5/1.12.2.1 cray-netcdf/4.9.0.1
+
+set -x
+export ESMF_NETCDF_LIBS="-lnetcdff -lnetcdf"
+export ESMF_DIR=/global/cfs/cdirs/e3sm/theurich/ESMF-Nightly-Testing/gfortran_11.2.0_mpi_g_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /global/cfs/cdirs/e3sm/theurich/ESMF-Nightly-Testing/gfortran_11.2.0_mpi_g_develop/module-test.log
+cd /global/cfs/cdirs/e3sm/theurich/ESMF-Nightly-Testing/gfortran_11.2.0_mpi_g_develop/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
