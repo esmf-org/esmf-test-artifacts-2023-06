bbd304884ee68bdd8159c79748cde1fde3a1a46c
Mark A Potts <Mark.Potts@noaa.gov>
2023-05-12 04:29:53 +0000

    action=collect dir=nvhpc_22.2_mpiuni_O_feature_fill_nan branch=feature_fill_nan hash=v8.5.0b18-9-g15f3b88 phase=build

---
+++ feature_fill_nan/nvhpc/22.2/O/mpiuni/None/out/build.bat
@@ -0,0 +1,29 @@
+#!/bin/sh -l
+#SBATCH --account=hfv3gfs
+#SBATCH -o /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/nvhpc_22.2_mpiuni_O_feature_fill_nan/build.bat_%j.o
+#SBATCH -e /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/nvhpc_22.2_mpiuni_O_feature_fill_nan/build.bat_%j.e
+#SBATCH --time=1:20:00
+#SBATCH --partition=xjet
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake
+module load nvhpc/22.2 
+
+set -x
+export ESMF_DIR=/lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/nvhpc_22.2_mpiuni_O_feature_fill_nan/esmf
+export ESMF_COMPILER=nvhpc
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/nvhpc_22.2_mpiuni_O_feature_fill_nan/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/nvhpc_22.2_mpiuni_O_feature_fill_nan/module-build.log
+cd /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/nvhpc_22.2_mpiuni_O_feature_fill_nan/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 24 2>&1| tee ../build.log
