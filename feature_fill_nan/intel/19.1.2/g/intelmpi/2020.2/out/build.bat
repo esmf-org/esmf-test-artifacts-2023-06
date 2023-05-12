a2542b4bcd925237473a4a7d26060cd5b70663d0
Mark A Potts <Mark.Potts@noaa.gov>
2023-05-12 04:10:54 +0000

    action=collect dir=intel_19.1.2_intelmpi_g_feature_fill_nan branch=feature_fill_nan hash=v8.5.0b18-9-g15f3b88 phase=build

---
+++ feature_fill_nan/intel/19.1.2/g/intelmpi/2020.2/out/build.bat
@@ -0,0 +1,31 @@
+#!/bin/sh -l
+#SBATCH --account=hfv3gfs
+#SBATCH -o /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/intel_19.1.2_intelmpi_g_feature_fill_nan/build.bat_%j.o
+#SBATCH -e /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/intel_19.1.2_intelmpi_g_feature_fill_nan/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=xjet
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake
+module load intel/2020.2 impi/2020.2
+module load netcdf/4.7.0
+
+set -x
+export ESMF_MPIRUN=mpirun.srun
+export ESMF_DIR=/lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/intel_19.1.2_intelmpi_g_feature_fill_nan/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/intel_19.1.2_intelmpi_g_feature_fill_nan/module-build.log
+cd /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/intel_19.1.2_intelmpi_g_feature_fill_nan/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 24 2>&1| tee ../build.log
