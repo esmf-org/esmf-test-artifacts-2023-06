3e8f58b0ae0ce9016e636cfcb8fb2735a6cc63f4
Robert Oehmke <oehmke@ucar.edu>
2023-05-13 10:00:22 -0600

    action=collect dir=gfortranclang_12.2.0_14.0.0_mpich_g_feature_fill_nan branch=feature_fill_nan hash=v8.5.0b18-9-g15f3b88 phase=test

---
+++ feature_fill_nan/gfortranclang/12.2.0_14.0.0/g/mpich/4.1.1/out/test.bat
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load gfortran-12.2.0_clang-14.0.0 4.1.1
+module load netcdf-4.9.2
+
+set -x
+export ESMF_DIR=/Users/oehmke/ESMF_AutoTest/gfortranclang_12.2.0_14.0.0_mpich_g_feature_fill_nan/esmf
+export ESMF_COMPILER=gfortranclang
+export ESMF_COMM=mpich
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /Users/oehmke/ESMF_AutoTest/gfortranclang_12.2.0_14.0.0_mpich_g_feature_fill_nan/module-test.log
+cd /Users/oehmke/ESMF_AutoTest/gfortranclang_12.2.0_14.0.0_mpich_g_feature_fill_nan/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
