f5d63109593ed664339dabc5fade63f9f2ca35c6
Bill Sacks <sacks@ucar.edu>
2023-05-14 03:16:46 -0600

    action=collect dir=gfortranclang_12.2.0_14.0.0_openmpi_g_feature_fill_nan branch=feature_fill_nan hash=v8.5.0b18-9-g15f3b88 phase=test

---
+++ feature_fill_nan/gfortranclang/12.2.0_14.0.0/g/openmpi/4.1.4/out/test.bat
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load gfortran-12.2.0_clang-14.0.0 4.1.4
+module load netcdf-4.9.0
+
+set -x
+export ESMF_DIR=/Users/sacks/projects/scratch/esmf-testing/gfortranclang_12.2.0_14.0.0_openmpi_g_feature_fill_nan/esmf
+export ESMF_COMPILER=gfortranclang
+export ESMF_COMM=openmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /Users/sacks/projects/scratch/esmf-testing/gfortranclang_12.2.0_14.0.0_openmpi_g_feature_fill_nan/module-test.log
+cd /Users/sacks/projects/scratch/esmf-testing/gfortranclang_12.2.0_14.0.0_openmpi_g_feature_fill_nan/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
