922d35941ef8c6011edeec8cca297fd2c59b6a27
Mark.Potts <Mark.Potts@noaa.gov>
2023-05-14 02:45:09 -0400

    action=collect dir=intel_19.1.3_intelmpi_g_feature_fill_nan branch=feature_fill_nan hash=v8.5.0b18-9-g15f3b88 phase=test

---
+++ feature_fill_nan/intel/19.1.3/g/intelmpi/19.1.3.304/out/test.bat
@@ -0,0 +1,31 @@
+#!/bin/sh -l
+#SBATCH --account=s2326
+#SBATCH -o /discover/nobackup/projects/sbu/mpotts/esmf-test/intel_19.1.3_intelmpi_g_feature_fill_nan/test.bat_%j.o
+#SBATCH -e /discover/nobackup/projects/sbu/mpotts/esmf-test/intel_19.1.3_intelmpi_g_feature_fill_nan/test.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load comp/intel/19.1.3.304 mpi/impi/19.1.3.304
+module load netcdf4/4.7.4
+
+set -x
+export ESMF_DIR=/discover/nobackup/projects/sbu/mpotts/esmf-test/intel_19.1.3_intelmpi_g_feature_fill_nan/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /discover/nobackup/projects/sbu/mpotts/esmf-test/intel_19.1.3_intelmpi_g_feature_fill_nan/module-test.log
+cd /discover/nobackup/projects/sbu/mpotts/esmf-test/intel_19.1.3_intelmpi_g_feature_fill_nan/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
