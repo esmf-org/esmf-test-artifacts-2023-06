bc86fd46281bb0f2843e2840042f91d242815028
Mark Potts <mpotts@redlineperf.com>
2023-05-15 03:41:46 -0500

    action=collect dir=gfortran_8.3.0_mpiuni_O_fix_profile_summary_at_scale branch=fix_profile_summary_at_scale hash=v8.5.0b20-7-g842a2b6 phase=test

---
+++ fix_profile_summary_at_scale/gfortran/8.3.0/O/mpiuni/None/out/test.bat
@@ -0,0 +1,32 @@
+#!/bin/sh -l
+#SBATCH --account=da-cpu
+#SBATCH -o /work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_O_fix_profile_summary_at_scale/test.bat_%j.o
+#SBATCH -e /work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_O_fix_profile_summary_at_scale/test.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=orion
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake intelpython3
+module load gcc/8.3.0 
+module load netcdf/4.7.4
+
+set -x
+export LD_PRELOAD=/apps/gcc-8/gcc-8.3.0/lib64/libstdc++.so
+export ESMF_DIR=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_O_fix_profile_summary_at_scale/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_O_fix_profile_summary_at_scale/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_O_fix_profile_summary_at_scale/module-test.log
+cd /work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_O_fix_profile_summary_at_scale/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
