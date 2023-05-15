b0f4587c4c155ac4bc44c3072a418158c155ed1b
Gerhard Theurich <theurich@sourcespring.net>
2023-05-15 09:03:05 +0000

    action=collect dir=pgi_19.1_mpiuni_O_fix_profile_summary_at_scale branch=fix_profile_summary_at_scale hash=v8.5.0b20-7-g842a2b6 phase=test

---
+++ fix_profile_summary_at_scale/pgi/19.1/O/mpiuni/None/out/test.bat
@@ -0,0 +1,29 @@
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/pgi_19.1_mpiuni_O_fix_profile_summary_at_scale/test.bat_%j.o
+#SBATCH -e /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/pgi_19.1_mpiuni_O_fix_profile_summary_at_scale/test.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake
+module load pgi/19.10 
+
+set -x
+export ESMF_DIR=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/pgi_19.1_mpiuni_O_fix_profile_summary_at_scale/esmf
+export ESMF_COMPILER=pgi
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/pgi_19.1_mpiuni_O_fix_profile_summary_at_scale/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/pgi_19.1_mpiuni_O_fix_profile_summary_at_scale/module-test.log
+cd /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/pgi_19.1_mpiuni_O_fix_profile_summary_at_scale/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
