59aabf150059c02093738881d07f26371800a460
Gerhard Theurich <theurich@sourcespring.net>
2023-05-15 05:02:47 -0600

    action=collect dir=nvhpc_22.2_mpiuni_O_fix_profile_summary_at_scale branch=fix_profile_summary_at_scale hash=v8.5.0b20-7-g842a2b6 phase=test

---
+++ fix_profile_summary_at_scale/nvhpc/22.2/O/mpiuni/None/out/build.bat
@@ -0,0 +1,26 @@
+#!/bin/sh -l
+#PBS -N build.bat
+#PBS -l walltime=1:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load python/3.7.9
+module load nvhpc/22.2 
+module load netcdf/4.8.1
+
+set -x
+export ESMF_DIR=/glade/scratch/theurich/ESMF-Nightly-Testing/nvhpc_22.2_mpiuni_O_fix_profile_summary_at_scale/esmf
+export ESMF_COMPILER=nvhpc
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/glade/scratch/theurich/ESMF-Nightly-Testing/nvhpc_22.2_mpiuni_O_fix_profile_summary_at_scale/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /glade/scratch/theurich/ESMF-Nightly-Testing/nvhpc_22.2_mpiuni_O_fix_profile_summary_at_scale/module-build.log
+cd /glade/scratch/theurich/ESMF-Nightly-Testing/nvhpc_22.2_mpiuni_O_fix_profile_summary_at_scale/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 36 2>&1| tee ../build.log
