3fff780cdde4f7a385c827ac96fb79ac3f6e76b9
Mark A Potts <Mark.Potts@noaa.gov>
2023-05-15 04:35:30 +0000

    action=collect dir=gfortran_9.2.0_openmpi_O_fix_profile_summary_at_scale branch=fix_profile_summary_at_scale hash=v8.5.0b20-7-g842a2b6 phase=test

---
+++ fix_profile_summary_at_scale/gfortran/9.2.0/O/openmpi/3.1.4/out/test.bat
@@ -0,0 +1,31 @@
+#!/bin/sh -l
+#SBATCH --account=hfv3gfs
+#SBATCH -o /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/gfortran_9.2.0_openmpi_O_fix_profile_summary_at_scale/test.bat_%j.o
+#SBATCH -e /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/gfortran_9.2.0_openmpi_O_fix_profile_summary_at_scale/test.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=xjet
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake
+module load gnu/9.2.0 openmpi/3.1.4
+
+set -x
+export ESMF_MPIRUN=mpirun.srun
+export ESMF_DIR=/lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/gfortran_9.2.0_openmpi_O_fix_profile_summary_at_scale/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/gfortran_9.2.0_openmpi_O_fix_profile_summary_at_scale/module-test.log
+cd /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/gfortran_9.2.0_openmpi_O_fix_profile_summary_at_scale/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
