1d2df85ed9aa52e011a4741642bc06095edb3843
Gerhard Theurich <theurich@sourcespring.net>
2023-05-15 08:19:25 +0000

    action=collect dir=intel_18.0.5_mpiuni_g_fix_profile_summary_at_scale branch=fix_profile_summary_at_scale hash=v8.5.0b20-7-g842a2b6 phase=test

---
+++ fix_profile_summary_at_scale/intel/18.0.5/g/mpiuni/None/out/build.bat
@@ -0,0 +1,31 @@
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_18.0.5_mpiuni_g_fix_profile_summary_at_scale/build.bat_%j.o
+#SBATCH -e /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_18.0.5_mpiuni_g_fix_profile_summary_at_scale/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake
+module load intel/18.0.5.274 
+module load netcdf/4.6.1
+
+set -x
+export ESMF_DIR=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_18.0.5_mpiuni_g_fix_profile_summary_at_scale/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_18.0.5_mpiuni_g_fix_profile_summary_at_scale/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_18.0.5_mpiuni_g_fix_profile_summary_at_scale/module-build.log
+cd /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_18.0.5_mpiuni_g_fix_profile_summary_at_scale/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 40 2>&1| tee ../build.log
