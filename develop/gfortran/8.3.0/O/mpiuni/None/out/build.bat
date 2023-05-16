f8de140865afbe97971c695ec9fc171d9e288bee
Mark A Potts <Mark.Potts@noaa.gov>
2023-05-16 00:52:49 -0400

    action=collect dir=gfortran_8.3.0_mpiuni_O_develop branch=develop hash=v8.5.0b21-32-g22c0612 phase=test

---
+++ develop/gfortran/8.3.0/O/mpiuni/None/out/build.bat
@@ -0,0 +1,33 @@
+#!/bin/sh -l
+#SBATCH --account=nggps_emc
+#SBATCH -o /lustre/f2/dev/Mark.Potts/esmf-test/gfortran_8.3.0_mpiuni_O_develop/build.bat_%j.o
+#SBATCH -e /lustre/f2/dev/Mark.Potts/esmf-test/gfortran_8.3.0_mpiuni_O_develop/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --cluster=c3
+#SBATCH --qos=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module unload PrgEnv-intel darshan
+module load PrgEnv-gnu git/2.26.0
+module load gcc/8.3.0 
+module load cray-netcdf/4.6.3.2
+
+set -x
+export ESMF_NETCDF_LIBS="-lnetcdff -lnetcdf"
+export ESMF_DIR=/lustre/f2/dev/Mark.Potts/esmf-test/gfortran_8.3.0_mpiuni_O_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/lustre/f2/dev/Mark.Potts/esmf-test/gfortran_8.3.0_mpiuni_O_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /lustre/f2/dev/Mark.Potts/esmf-test/gfortran_8.3.0_mpiuni_O_develop/module-build.log
+cd /lustre/f2/dev/Mark.Potts/esmf-test/gfortran_8.3.0_mpiuni_O_develop/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 24 2>&1| tee ../build.log
