ec991a535857d081da5b79485950ca44f90657d6
Gerhard Theurich <theurich@sourcespring.net>
2023-05-15 20:46:40 -0400

    action=collect dir=gfortran_12.2.0_mpiuni_O_feature_state-intent-internal branch=feature_state-intent-internal hash=v8.5.0b21-24-g107dc0b phase=test

---
+++ feature_state-intent-internal/gfortran/12.2.0/O/mpiuni/None/out/test.bat
@@ -0,0 +1,33 @@
+#!/bin/sh -l
+#SBATCH --account=esrl_bmcs
+#SBATCH -o /lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C5/gfortran_12.2.0_mpiuni_O_feature_state-intent-internal/test.bat_%j.o
+#SBATCH -e /lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C5/gfortran_12.2.0_mpiuni_O_feature_state-intent-internal/test.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --cluster=c5
+#SBATCH --qos=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module unload darshan-runtime PrgEnv-intel cray-mpich
+module load PrgEnv-gnu git
+module load gcc/12.2.0 
+module load cray-hdf5/1.12.2.3 cray-netcdf/4.9.0.3
+
+set -x
+export ESMF_NETCDF_LIBS="-lnetcdff -lnetcdf"
+export ESMF_DIR=/lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C5/gfortran_12.2.0_mpiuni_O_feature_state-intent-internal/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C5/gfortran_12.2.0_mpiuni_O_feature_state-intent-internal/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C5/gfortran_12.2.0_mpiuni_O_feature_state-intent-internal/module-test.log
+cd /lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C5/gfortran_12.2.0_mpiuni_O_feature_state-intent-internal/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
