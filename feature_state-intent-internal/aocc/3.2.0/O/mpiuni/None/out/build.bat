61bea64bd5aa647ad313d8583063b9ac1d105913
Gerhard Theurich <theurich@sourcespring.net>
2023-05-15 21:37:12 -0700

    action=collect dir=aocc_3.2.0_mpiuni_O_feature_state-intent-internal branch=feature_state-intent-internal hash=v8.5.0b21-37-g0d21bb7 phase=build

---
+++ feature_state-intent-internal/aocc/3.2.0/O/mpiuni/None/out/build.bat
@@ -0,0 +1,32 @@
+#!/bin/sh -l
+#SBATCH --account=e3sm
+#SBATCH -o /global/cfs/cdirs/e3sm/theurich/ESMF-Nightly-Testing/aocc_3.2.0_mpiuni_O_feature_state-intent-internal/build.bat_%j.o
+#SBATCH -e /global/cfs/cdirs/e3sm/theurich/ESMF-Nightly-Testing/aocc_3.2.0_mpiuni_O_feature_state-intent-internal/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH -C cpu
+#SBATCH --qos=regular
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load PrgEnv-aocc cpu
+module load aocc/3.2.0 
+module load cray-hdf5/1.12.2.1 cray-netcdf/4.9.0.1
+
+set -x
+export ESMF_NETCDF_LIBS="-lnetcdff -lnetcdf"
+export ESMF_DIR=/global/cfs/cdirs/e3sm/theurich/ESMF-Nightly-Testing/aocc_3.2.0_mpiuni_O_feature_state-intent-internal/esmf
+export ESMF_COMPILER=aocc
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/global/cfs/cdirs/e3sm/theurich/ESMF-Nightly-Testing/aocc_3.2.0_mpiuni_O_feature_state-intent-internal/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /global/cfs/cdirs/e3sm/theurich/ESMF-Nightly-Testing/aocc_3.2.0_mpiuni_O_feature_state-intent-internal/module-build.log
+cd /global/cfs/cdirs/e3sm/theurich/ESMF-Nightly-Testing/aocc_3.2.0_mpiuni_O_feature_state-intent-internal/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 128 2>&1| tee ../build.log
