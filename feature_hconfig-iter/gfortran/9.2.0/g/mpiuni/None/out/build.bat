53278eb39a7c70f8e0c6318b9cecdb33170a2615
Gerhard Theurich <theurich@sourcespring.net>
2023-05-16 22:38:20 +0000

    action=collect dir=gfortran_9.2.0_mpiuni_g_feature_hconfig-iter branch=feature_hconfig-iter hash=v8.5.0b21-122-ge977c88 phase=test

---
+++ feature_hconfig-iter/gfortran/9.2.0/g/mpiuni/None/out/build.bat
@@ -0,0 +1,31 @@
+#!/bin/sh -l
+#SBATCH --account=esmf
+#SBATCH -o /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0_mpiuni_g_feature_hconfig-iter/build.bat_%j.o
+#SBATCH -e /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0_mpiuni_g_feature_hconfig-iter/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake
+module load gnu/9.2.0 
+module load netcdf/4.7.2
+
+set -x
+export ESMF_DIR=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0_mpiuni_g_feature_hconfig-iter/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0_mpiuni_g_feature_hconfig-iter/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0_mpiuni_g_feature_hconfig-iter/module-build.log
+cd /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0_mpiuni_g_feature_hconfig-iter/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 40 2>&1| tee ../build.log
