7811154e9952fde1ad5be93920e50feb94fa7daa
Gerhard Theurich <theurich@sourcespring.net>
2023-05-16 20:39:59 -0400

    action=collect dir=intel_2022.2.1-classic_mpi_O_develop branch=develop hash=v8.5.0b21-43-g2bcd9b7 phase=test

---
+++ develop/intel/2022.2.1-classic/O/mpi/7.7.20/out/test.bat
@@ -0,0 +1,32 @@
+#!/bin/sh -l
+#SBATCH --account=esrl_bmcs
+#SBATCH -o /lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C3_4/intel_2022.2.1-classic_mpi_O_develop/test.bat_%j.o
+#SBATCH -e /lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C3_4/intel_2022.2.1-classic_mpi_O_develop/test.bat_%j.e
+#SBATCH --time=3:00:00
+#SBATCH --cluster=c4
+#SBATCH --qos=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module unload darshan
+module load intel-classic/2022.2.1 cray-mpich/7.7.20
+module load cray-hdf5/1.12.2.3 cray-netcdf/4.9.0.3
+
+set -x
+export ESMF_DIR=/lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C3_4/intel_2022.2.1-classic_mpi_O_develop/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C3_4/intel_2022.2.1-classic_mpi_O_develop/module-test.log
+cd /lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C3_4/intel_2022.2.1-classic_mpi_O_develop/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
