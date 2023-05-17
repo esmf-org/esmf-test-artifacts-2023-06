b8c60d386aca466de98ef897b455d0c7010afdbe
Gerhard Theurich <theurich@sourcespring.net>
2023-05-17 02:12:54 -0700

    action=collect dir=gfortranclang_7.5.0_13.0.0_mpiuni_O_develop branch=develop hash=v8.5.0b21-43-g2bcd9b7 phase=test

---
+++ develop/gfortranclang/7.5.0_13.0.0/O/mpiuni/None/out/test.bat
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load AOCC/3.2.0 
+
+set -x
+export ESMF_DIR=/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_13.0.0_mpiuni_O_develop/esmf
+export ESMF_COMPILER=gfortranclang
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_13.0.0_mpiuni_O_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_13.0.0_mpiuni_O_develop/module-test.log
+cd /home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_13.0.0_mpiuni_O_develop/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
