d55db9ed765cafb54de9b84e34d37e907675e640
Gerhard Theurich <theurich@sourcespring.net>
2023-05-18 12:27:37 -0400

    action=collect dir=gfortran_12.1.0_mpiuni_g_develop branch=develop hash=v8.5.0b21-127-g2a7bdbf phase=test

---
+++ develop/gfortran/12.1.0/g/mpiuni/None/out/build.bat
@@ -0,0 +1,35 @@
+#!/bin/sh -l
+#SBATCH --account=esrl_bmcs
+#SBATCH -o /lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C3_4/gfortran_12.1.0_mpiuni_g_develop/build.bat_%j.o
+#SBATCH -e /lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C3_4/gfortran_12.1.0_mpiuni_g_develop/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --cluster=c4
+#SBATCH --qos=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module unload PrgEnv-intel darshan
+module load PrgEnv-gnu
+module load gcc/12.1.0 
+module load cray-hdf5/1.12.1.3 cray-netcdf/4.8.1.3
+
+set -x
+export ESMF_NETCDF_INCLUDE="$CRAY_NETCDF_PREFIX/include"
+export ESMF_NETCDF_LIBPATH="$CRAY_NETCDF_PREFIX/lib"
+export ESMF_NETCDF_LIBS="-lnetcdff -lnetcdf"
+export ESMF_DIR=/lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C3_4/gfortran_12.1.0_mpiuni_g_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C3_4/gfortran_12.1.0_mpiuni_g_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C3_4/gfortran_12.1.0_mpiuni_g_develop/module-build.log
+cd /lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C3_4/gfortran_12.1.0_mpiuni_g_develop/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 24 2>&1| tee ../build.log
