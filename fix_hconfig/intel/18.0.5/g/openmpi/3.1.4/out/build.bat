dd023269c7f8f2504f1cc62fd85b0bd9a29eb851
Gerhard Theurich <theurich@sourcespring.net>
2023-05-18 16:14:48 -0600

    action=collect dir=intel_18.0.5_openmpi_g_fix_hconfig branch=fix_hconfig hash=v8.5.0b21-129-g9059f04 phase=test

---
+++ fix_hconfig/intel/18.0.5/g/openmpi/3.1.4/out/build.bat
@@ -0,0 +1,26 @@
+#!/bin/sh -l
+#PBS -N build.bat
+#PBS -l walltime=2:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load intel/18.0.5 openmpi/3.1.4
+module load netcdf/4.6.3
+module load python/3.7.9
+
+set -x
+export ESMPY_DATA_DIR="/glade/work/theurich/esmf-test-data/grids"
+export ESMF_DIR=/glade/scratch/theurich/ESMF-Nightly-Testing/intel_18.0.5_openmpi_g_fix_hconfig/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=openmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /glade/scratch/theurich/ESMF-Nightly-Testing/intel_18.0.5_openmpi_g_fix_hconfig/module-build.log
+cd /glade/scratch/theurich/ESMF-Nightly-Testing/intel_18.0.5_openmpi_g_fix_hconfig/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 36 2>&1| tee ../build.log
