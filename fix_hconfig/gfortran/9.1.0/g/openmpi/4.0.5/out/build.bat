1d96e5b1415712219a053bf40c995ee1625ca381
Gerhard Theurich <theurich@sourcespring.net>
2023-05-19 15:16:50 -0600

    action=collect dir=gfortran_9.1.0_openmpi_g_fix_hconfig branch=fix_hconfig hash=v8.5.0b21-131-g7f2cf84 phase=build

---
+++ fix_hconfig/gfortran/9.1.0/g/openmpi/4.0.5/out/build.bat
@@ -0,0 +1,25 @@
+#!/bin/sh -l
+#PBS -N build.bat
+#PBS -l walltime=1:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load python/3.7.9
+module load gnu/9.1.0 openmpi/4.0.5
+module load netcdf/4.7.3
+
+set -x
+export ESMF_DIR=/glade/scratch/theurich/ESMF-Nightly-Testing/gfortran_9.1.0_openmpi_g_fix_hconfig/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /glade/scratch/theurich/ESMF-Nightly-Testing/gfortran_9.1.0_openmpi_g_fix_hconfig/module-build.log
+cd /glade/scratch/theurich/ESMF-Nightly-Testing/gfortran_9.1.0_openmpi_g_fix_hconfig/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 36 2>&1| tee ../build.log
