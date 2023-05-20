abf59fc5301ccb4a0eb53beed0d462d596d22393
Gerhard Theurich <theurich@sourcespring.net>
2023-05-20 05:40:47 -0700

    action=collect dir=gfortran_11.2.0_mpich_g_develop branch=develop hash=v8.5.0b21-130-gf20d586 phase=test

---
+++ develop/gfortran/11.2.0/g/mpich/4.0.0/out/test.bat
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load GCC/11.2.0/GCC GCC/11.2.0/mpich/4.0.0
+module load GCC/11.2.0/netcdf/4.7.4-mpich4.0.0
+
+set -x
+export ESMF_DIR=/home/gerhard/ESMF-Nightly-Testing/gfortran_11.2.0_mpich_g_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpich
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /home/gerhard/ESMF-Nightly-Testing/gfortran_11.2.0_mpich_g_develop/module-test.log
+cd /home/gerhard/ESMF-Nightly-Testing/gfortran_11.2.0_mpich_g_develop/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
