22d44fc7ad371b91f29f0cc5c4a31b3acf55769d
Mark Potts <mpotts@redlineperf.com>
2023-06-09 03:07:26 -0500

    action=collect dir=intel_2020.2_intelmpi_g_feature_bopt_g_flags branch=feature_bopt_g_flags hash=v8.5.0b23-4-g91b6f9c phase=test

---
+++ feature_bopt_g_flags/intel/2020.2/g/intelmpi/2020.2/out/build.bat
@@ -0,0 +1,31 @@
+#!/bin/sh -l
+#SBATCH --account=da-cpu
+#SBATCH -o /work/noaa/da/mpotts/sandbox/intel_2020.2_intelmpi_g_feature_bopt_g_flags/build.bat_%j.o
+#SBATCH -e /work/noaa/da/mpotts/sandbox/intel_2020.2_intelmpi_g_feature_bopt_g_flags/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=orion
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake intelpython3
+module load intel/2020.2 impi/2020.2
+module load netcdf/4.7.4
+
+set -x
+export ESMF_MPIRUN=mpirun.srun
+export ESMF_DIR=/work/noaa/da/mpotts/sandbox/intel_2020.2_intelmpi_g_feature_bopt_g_flags/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /work/noaa/da/mpotts/sandbox/intel_2020.2_intelmpi_g_feature_bopt_g_flags/module-build.log
+cd /work/noaa/da/mpotts/sandbox/intel_2020.2_intelmpi_g_feature_bopt_g_flags/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 40 2>&1| tee ../build.log
