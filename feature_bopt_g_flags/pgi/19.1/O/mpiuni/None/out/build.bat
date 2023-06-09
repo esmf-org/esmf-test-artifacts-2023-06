83cd0b01ae9920130fff9e4837487374454d0fee
Gerhard Theurich <theurich@sourcespring.net>
2023-06-09 07:58:48 +0000

    action=collect dir=pgi_19.1_mpiuni_O_feature_bopt_g_flags branch=feature_bopt_g_flags hash=v8.5.0b23-4-g91b6f9c phase=build

---
+++ feature_bopt_g_flags/pgi/19.1/O/mpiuni/None/out/build.bat
@@ -0,0 +1,29 @@
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/pgi_19.1_mpiuni_O_feature_bopt_g_flags/build.bat_%j.o
+#SBATCH -e /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/pgi_19.1_mpiuni_O_feature_bopt_g_flags/build.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake
+module load pgi/19.10 
+
+set -x
+export ESMF_DIR=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/pgi_19.1_mpiuni_O_feature_bopt_g_flags/esmf
+export ESMF_COMPILER=pgi
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/pgi_19.1_mpiuni_O_feature_bopt_g_flags/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/pgi_19.1_mpiuni_O_feature_bopt_g_flags/module-build.log
+cd /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/pgi_19.1_mpiuni_O_feature_bopt_g_flags/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 40 2>&1| tee ../build.log
