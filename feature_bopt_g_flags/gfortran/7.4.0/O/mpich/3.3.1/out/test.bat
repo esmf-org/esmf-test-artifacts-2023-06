f9368ef39f3f92a1f57ebe91133831f7a5b0b0be
Gerhard Theurich <theurich@sourcespring.net>
2023-06-10 03:26:19 -0700

    action=collect dir=gfortran_7.4.0_mpich_O_feature_bopt_g_flags branch=feature_bopt_g_flags hash=v8.5.0b23-7-gd659dab phase=test

---
+++ feature_bopt_g_flags/gfortran/7.4.0/O/mpich/3.3.1/out/test.bat
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load GCC/7.4.0/GCC GCC/7.4.0/mpich/3.3.1
+module load GCC/7.4.0/netcdf/4.7.4p
+
+set -x
+export ESMF_DIR=/home/gerhard/ESMF-Nightly-Testing/gfortran_7.4.0_mpich_O_feature_bopt_g_flags/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpich
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /home/gerhard/ESMF-Nightly-Testing/gfortran_7.4.0_mpich_O_feature_bopt_g_flags/module-test.log
+cd /home/gerhard/ESMF-Nightly-Testing/gfortran_7.4.0_mpich_O_feature_bopt_g_flags/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
