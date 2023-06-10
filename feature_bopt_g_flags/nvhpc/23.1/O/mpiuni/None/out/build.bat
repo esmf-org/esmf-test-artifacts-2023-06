4a453629167d74fe14cae820882df024a2b027b4
Gerhard Theurich <theurich@sourcespring.net>
2023-06-10 02:32:45 -0600

    action=collect dir=nvhpc_23.1_mpiuni_O_feature_bopt_g_flags branch=feature_bopt_g_flags hash=v8.5.0b23-7-gd659dab phase=build

---
+++ feature_bopt_g_flags/nvhpc/23.1/O/mpiuni/None/out/build.bat
@@ -0,0 +1,25 @@
+#!/bin/sh -l
+#PBS -N build.bat
+#PBS -l walltime=2:00:00
+#PBS -q main
+#PBS -A p93300606
+#PBS -l select=1:ncpus=128:mpiprocs=128
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load nvhpc/23.1 
+module load netcdf/4.9.1
+
+set -x
+export ESMF_DIR=/glade/gust/scratch/theurich/ESMF-Nightly-Testing/nvhpc_23.1_mpiuni_O_feature_bopt_g_flags/esmf
+export ESMF_COMPILER=nvhpc
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/glade/gust/scratch/theurich/ESMF-Nightly-Testing/nvhpc_23.1_mpiuni_O_feature_bopt_g_flags/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /glade/gust/scratch/theurich/ESMF-Nightly-Testing/nvhpc_23.1_mpiuni_O_feature_bopt_g_flags/module-build.log
+cd /glade/gust/scratch/theurich/ESMF-Nightly-Testing/nvhpc_23.1_mpiuni_O_feature_bopt_g_flags/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 128 2>&1| tee ../build.log
