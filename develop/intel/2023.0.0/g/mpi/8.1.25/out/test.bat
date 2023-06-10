f525403d152f2b395b8dabb406c32c77a7cabee3
Gerhard Theurich <theurich@sourcespring.net>
2023-06-10 02:02:25 -0600

    action=collect dir=intel_2023.0.0_mpi_g_develop branch=develop hash=v8.5.0b23-2-g2d76824 phase=test

---
+++ develop/intel/2023.0.0/g/mpi/8.1.25/out/test.bat
@@ -0,0 +1,27 @@
+#!/bin/sh -l
+#PBS -N test.bat
+#PBS -l walltime=2:00:00
+#PBS -q main
+#PBS -A ACGD0008
+#PBS -l select=1:ncpus=128:mpiprocs=128
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load intel/2023.0.0 cray-mpich/8.1.25
+module load netcdf/4.9.1
+
+set -x
+export ESMF_MPIRUN=mpiexec
+export ESMF_DIR=/glade/derecho/scratch/theurich/ESMF-Nightly-Testing/intel_2023.0.0_mpi_g_develop/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /glade/derecho/scratch/theurich/ESMF-Nightly-Testing/intel_2023.0.0_mpi_g_develop/module-test.log
+cd /glade/derecho/scratch/theurich/ESMF-Nightly-Testing/intel_2023.0.0_mpi_g_develop/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
