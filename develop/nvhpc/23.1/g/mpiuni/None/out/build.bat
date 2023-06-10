c07c2ff2c4f0fa16e780984d49ca8edea2e284b5
Gerhard Theurich <theurich@sourcespring.net>
2023-06-10 03:00:16 -0600

    action=collect dir=nvhpc_23.1_mpiuni_g_develop branch=develop hash=v8.5.0b23-2-g2d76824 phase=test

---
+++ develop/nvhpc/23.1/g/mpiuni/None/out/build.bat
@@ -0,0 +1,25 @@
+#!/bin/sh -l
+#PBS -N build.bat
+#PBS -l walltime=2:00:00
+#PBS -q main
+#PBS -A ACGD0008
+#PBS -l select=1:ncpus=128:mpiprocs=128
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load nvhpc/23.1 
+module load netcdf/4.9.1
+
+set -x
+export ESMF_DIR=/glade/derecho/scratch/theurich/ESMF-Nightly-Testing/nvhpc_23.1_mpiuni_g_develop/esmf
+export ESMF_COMPILER=nvhpc
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/glade/derecho/scratch/theurich/ESMF-Nightly-Testing/nvhpc_23.1_mpiuni_g_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /glade/derecho/scratch/theurich/ESMF-Nightly-Testing/nvhpc_23.1_mpiuni_g_develop/module-build.log
+cd /glade/derecho/scratch/theurich/ESMF-Nightly-Testing/nvhpc_23.1_mpiuni_g_develop/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 128 2>&1| tee ../build.log
