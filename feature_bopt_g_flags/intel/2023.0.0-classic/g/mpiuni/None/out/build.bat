0be45db18196a1c4353e004b38b549d74cd14890
Gerhard Theurich <theurich@sourcespring.net>
2023-06-11 02:14:59 -0600

    action=collect dir=intel_2023.0.0-classic_mpiuni_g_feature_bopt_g_flags branch=feature_bopt_g_flags hash=v8.5.0b23-7-gd659dab phase=test

---
+++ feature_bopt_g_flags/intel/2023.0.0-classic/g/mpiuni/None/out/build.bat
@@ -0,0 +1,25 @@
+#!/bin/sh -l
+#PBS -N build.bat
+#PBS -l walltime=2:00:00
+#PBS -q main
+#PBS -A p93300606
+#PBS -l select=1:ncpus=128:mpiprocs=128
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load intel-classic/2023.0.0 
+module load netcdf/4.9.1
+
+set -x
+export ESMF_DIR=/glade/gust/scratch/theurich/ESMF-Nightly-Testing/intel_2023.0.0-classic_mpiuni_g_feature_bopt_g_flags/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/glade/gust/scratch/theurich/ESMF-Nightly-Testing/intel_2023.0.0-classic_mpiuni_g_feature_bopt_g_flags/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /glade/gust/scratch/theurich/ESMF-Nightly-Testing/intel_2023.0.0-classic_mpiuni_g_feature_bopt_g_flags/module-build.log
+cd /glade/gust/scratch/theurich/ESMF-Nightly-Testing/intel_2023.0.0-classic_mpiuni_g_feature_bopt_g_flags/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 128 2>&1| tee ../build.log
