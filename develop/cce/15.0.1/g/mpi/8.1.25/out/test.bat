5a99077c1340ca3289a3053b20b22aff89e4ca2c
Gerhard Theurich <theurich@sourcespring.net>
2023-06-12 03:33:03 -0600

    action=collect dir=cce_15.0.1_mpi_g_develop branch=develop hash=v8.5.0b23-4-g4ad8e23 phase=test

---
+++ develop/cce/15.0.1/g/mpi/8.1.25/out/test.bat
@@ -0,0 +1,27 @@
+#!/bin/sh -l
+#PBS -N test.bat
+#PBS -l walltime=2:00:00
+#PBS -q main
+#PBS -A p93300606
+#PBS -l select=1:ncpus=128:mpiprocs=128
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load cce/15.0.1 cray-mpich/8.1.25
+module load netcdf/4.9.1
+
+set -x
+export ESMF_MPIRUN=mpiexec
+export ESMF_DIR=/glade/gust/scratch/theurich/ESMF-Nightly-Testing/cce_15.0.1_mpi_g_develop/esmf
+export ESMF_COMPILER=cce
+export ESMF_COMM=mpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /glade/gust/scratch/theurich/ESMF-Nightly-Testing/cce_15.0.1_mpi_g_develop/module-test.log
+cd /glade/gust/scratch/theurich/ESMF-Nightly-Testing/cce_15.0.1_mpi_g_develop/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
