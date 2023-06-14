62b43fd9a0486ffe2898a9035f405f8bcae77f53
Gerhard Theurich <theurich@sourcespring.net>
2023-06-14 01:59:28 -0600

    action=collect dir=intel_2023.0.0-classic_mpiuni_g_develop branch=develop hash=v8.5.0b23-6-gf97e9e9 phase=test

---
+++ develop/intel/2023.0.0-classic/g/mpiuni/None/out/test.bat
@@ -0,0 +1,25 @@
+#!/bin/sh -l
+#PBS -N test.bat
+#PBS -l walltime=2:00:00
+#PBS -q main
+#PBS -A ACGD0008
+#PBS -l select=1:ncpus=128:mpiprocs=128
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load intel-classic/2023.0.0 
+module load netcdf/4.9.1
+
+set -x
+export ESMF_DIR=/glade/derecho/scratch/theurich/ESMF-Nightly-Testing/intel_2023.0.0-classic_mpiuni_g_develop/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/glade/derecho/scratch/theurich/ESMF-Nightly-Testing/intel_2023.0.0-classic_mpiuni_g_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /glade/derecho/scratch/theurich/ESMF-Nightly-Testing/intel_2023.0.0-classic_mpiuni_g_develop/module-test.log
+cd /glade/derecho/scratch/theurich/ESMF-Nightly-Testing/intel_2023.0.0-classic_mpiuni_g_develop/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
