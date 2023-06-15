806b112f851cead305feb75b7cc8e95b2668b0e0
Mark A Potts <Mark.Potts@noaa.gov>
2023-06-15 05:35:41 +0000

    action=collect dir=nvhpc_23.3_openmpi_O_develop branch=develop hash=v8.5.0b23-6-gf97e9e9 phase=test

---
+++ develop/nvhpc/23.3/O/openmpi/23.3/out/test.bat
@@ -0,0 +1,31 @@
+#!/bin/sh -l
+#SBATCH --account=hfv3gfs
+#SBATCH -o /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/nvhpc_23.3_openmpi_O_develop/test.bat_%j.o
+#SBATCH -e /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/nvhpc_23.3_openmpi_O_develop/test.bat_%j.e
+#SBATCH --time=1:30:00
+#SBATCH --partition=xjet
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake
+module load nvhpc/23.3 nvhpc/23.3
+
+set -x
+export ESMF_MPIRUN=mpirun.srun
+export ESMF_DIR=/lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/nvhpc_23.3_openmpi_O_develop/esmf
+export ESMF_COMPILER=nvhpc
+export ESMF_COMM=openmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/nvhpc_23.3_openmpi_O_develop/module-test.log
+cd /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests/nvhpc_23.3_openmpi_O_develop/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
