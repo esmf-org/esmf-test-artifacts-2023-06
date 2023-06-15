65f59cb13a71b5ee2a503e40a8b8a0975231b340
Gerhard Theurich <theurich@sourcespring.net>
2023-06-15 03:03:24 -0700

    action=collect dir=gfortran_7.4.0_mpich_g_develop branch=develop hash=v8.5.0b23-6-gf97e9e9 phase=build

---
+++ develop/gfortran/7.4.0/g/mpich/3.3.1/out/build.bat
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load GCC/7.4.0/GCC GCC/7.4.0/mpich/3.3.1
+module load GCC/7.4.0/netcdf/4.7.4p
+
+set -x
+export ESMF_DIR=/home/gerhard/ESMF-Nightly-Testing/gfortran_7.4.0_mpich_g_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpich
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /home/gerhard/ESMF-Nightly-Testing/gfortran_7.4.0_mpich_g_develop/module-build.log
+cd /home/gerhard/ESMF-Nightly-Testing/gfortran_7.4.0_mpich_g_develop/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 4 2>&1| tee ../build.log
