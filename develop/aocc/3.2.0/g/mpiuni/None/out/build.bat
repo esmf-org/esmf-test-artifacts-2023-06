085bf31b61eca492b878924b3ac490a1680e05fd
Gerhard Theurich <theurich@sourcespring.net>
2023-06-15 01:30:16 -0700

    action=collect dir=aocc_3.2.0_mpiuni_g_develop branch=develop hash=v8.5.0b23-6-gf97e9e9 phase=test

---
+++ develop/aocc/3.2.0/g/mpiuni/None/out/build.bat
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load AOCC/3.2.0 
+
+set -x
+export ESMF_DIR=/home/gerhard/ESMF-Nightly-Testing/aocc_3.2.0_mpiuni_g_develop/esmf
+export ESMF_COMPILER=aocc
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/home/gerhard/ESMF-Nightly-Testing/aocc_3.2.0_mpiuni_g_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /home/gerhard/ESMF-Nightly-Testing/aocc_3.2.0_mpiuni_g_develop/module-build.log
+cd /home/gerhard/ESMF-Nightly-Testing/aocc_3.2.0_mpiuni_g_develop/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 4 2>&1| tee ../build.log
